6e1da0e7c66a87d95f0591cebb6891fd4d6b6515
José Alberto González Angulo <jose.gonzalez@blueoceantech.com.mx>
2019-10-22 12:39:13 -0500

    Proyecto Inical

---
+++ Changarro/ChangarroUser.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChangarroUser.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
