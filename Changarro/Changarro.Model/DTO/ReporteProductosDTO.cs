d848ca223eb01aecd1b07fc38677519b85d9d55c
GinaHerrera <56738788+GinaHerrera@users.noreply.github.com>
2019-12-06 15:44:07 -0600

    Gráficas realizadas: Productos por categoría y Clientes con más compras.

---
+++ Changarro/Changarro.Model/DTO/ReporteProductosDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Changarro.Model.DTO
+{
+    /// <summary>
+    /// Esta clase contiene 
+    /// </summary>
+   public class ReporteProductosDTO
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int iIdProducto { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int iIdDetalleCompra { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int iCantidad{ get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string cNombre{ get; set; }
+
+        
+    }
+}
